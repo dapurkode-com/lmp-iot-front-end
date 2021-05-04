96e83f9c46cb8011ffa9a565bbe7fa7bc2882b71
pandenengah <43629678+pandenengah@users.noreply.github.com>
2021-05-04 21:23:02 +0800

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './contents/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  urlMenu = {
+    dashboard: {
+      link: '/',
+      status: '',
+    },
+  }
+
+  constructor(private router: Router) {
+    this.urlMenuActivation()
+  }
+
+  ngOnInit(): void {
+  }
+
+  urlMenuActivation(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.keys(this.urlMenu).forEach(key => {
+          this.urlMenu[key].status = ''
+          if (event.url == this.urlMenu[key].link) {
+            this.urlMenu[key].status = 'active'
+          }
+        })
+      }
+    });
+  }
+
+  keys<O extends object>(obj: O): Array<keyof O> {
+    return Object.keys(obj) as Array<keyof O>
+  }
+
+}
