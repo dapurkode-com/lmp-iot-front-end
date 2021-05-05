894c24bbd75c3204b0dbc438b0c0cffef70376a8
wisnusudewa <76235208+wisnusudewa@users.noreply.github.com>
2021-05-05 22:36:24 +0800

    fourth

--- src/app/contents/dashboard/dash-healthlink/dash-healthlink.component.ts
+++ src/app/contents/dashboard/dash-healthlink/dash-healthlink.component.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import * as npm from '../../../../../package.json';
+import { Component, OnDestroy, OnInit, } from '@angular/core';
+import * as moment from 'moment';
+import global from '../../../_file/app.json';
 
 @Component({
   selector: 'app-dash-healthlink',
   templateUrl: './dash-healthlink.component.html',
   styleUrls: ['./dash-healthlink.component.scss']
 })
-export class DashHealthlinkComponent implements OnInit {
-  calorieObj = {}
+export class DashHealthlinkComponent implements OnInit, OnDestroy {
+  calorieObj = {
+    dateReadable: null,
+    calorieInKilo: null,
+  }
+  timeInterval: any
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(npm.global.api_url);
-    this.getData()
+    this.calorieObj = JSON.parse('' + localStorage.getItem(global.string_key.calorieObj))
+    this.setCalorieObj()
+
+    this.timeInterval =  setInterval(() => {
+      this.setCalorieObj()
+    }, 5000)
   }
 
-  getData(): void {
-    fetch(`${npm.global.api_url}calorie-expended/today`)
+  ngOnDestroy(): void {
+    clearInterval(this.timeInterval)
+    localStorage.setItem(global.string_key.calorieObj, JSON.stringify(this.calorieObj))
+  }
+
+  setCalorieObj(): void {
+    fetch(`${global.api_url}calorie-expended/today`)
       .then(response => response.json())
       .then(data => {
-        data.calorieInKilo = (data.calorie / 1000).toFixed(2);
+        data.calorieInKilo = (data.calorie / 1000).toFixed(2)
+        data.dateReadable = moment(data.datetime).format('DD MMMM YYYY, h:mmA')
         this.calorieObj = data
       });
   }
