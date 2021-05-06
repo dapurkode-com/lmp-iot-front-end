de7893255f796cd21cac6ec961a2ef09f2042104
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2021-05-06 11:39:49 +0800

    general restructure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lpm-itb-front'`, () => {
+  it(`should have as title 'lmp-itb-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lpm-itb-front');
+    expect(app.title).toEqual('lmp-itb-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lpm-itb-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('lmp-itb-front app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lpm-itb-front';
+  title = 'lmp-itb-front';
 }
