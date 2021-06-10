0ec862130d475c3db002525a09b664e1599dd27d
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2021-06-10 13:34:40 +0800

    health mood

--- src/app/contents/healthlink/health-mood/health-mood.component.ts
+++ src/app/contents/healthlink/health-mood/health-mood.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import global from '../../../_file/app.json';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-health-mood',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthMoodComponent implements OnInit {
 
+  heartRateObj = {
+    dateReadable: null,
+    rate: null,
+    happy: null,
+    neutral: null,
+    sad: null,
+    mood_text: null
+  }
+
+  timeInterval: any
+
   constructor() { }
 
   ngOnInit(): void {
+    this.heartRateObj = JSON.parse('' + localStorage.getItem(global.string_key.heartRateJson))
+
+    this.setHeartRateObj()
+    this.timeInterval = setInterval(() => {
+      this.setHeartRateObj()
+    }, 1000 * 60)
   }
 
+  ngOnDestroy(): void {
+    clearInterval(this.timeInterval)
+    localStorage.setItem(global.string_key.heartRateJson, JSON.stringify(this.heartRateObj))
+  }
+
+  setHeartRateObj(): void {
+    fetch(`${global.api_url}heart-rate/today`)
+      .then(response => response.json())
+      .then(data => {
+        data.dateReadable = moment(data.datetime).format('DD MMMM YYYY, h:mmA')
+        this.heartRateObj = data
+      });
+  }
 }
